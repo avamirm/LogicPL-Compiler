4a695e7d24ce4f7c2171976c630c0499c6f35fe5
Ava <avamir80@gmail.com>
2023-05-13 23:33:13 +0330

    add NameAnalyzer for loop, argDeclaration, implicationStmt, and arrayDeclaration

--- phase_2/src/visitor/nameAnalyzer/NameAnalyzer.java
+++ phase_2/src/visitor/nameAnalyzer/NameAnalyzer.java
@@ -2,6 +2,9 @@ package visitor.nameAnalyzer;
 
 import ast.node.Program;
 import ast.node.declaration.*;
+import ast.node.statement.ArrayDecStmt;
+import ast.node.statement.ForloopStmt;
+import ast.node.statement.ImplicationStmt;
 import ast.node.statement.VarDecStmt;
 import compileError.*;
 import compileError.Name.*;
@@ -16,20 +19,23 @@ import java.util.ArrayList;
 public class NameAnalyzer extends Visitor<Void> {
 
     public ArrayList<CompileError> nameErrors = new ArrayList<>();
-    //
     private static int counter = 0;
-    //
+
     @Override
     public Void visit(Program program) {
         SymbolTable.root = new SymbolTable();
         SymbolTable.push(SymbolTable.root);
 
-        for (FuncDeclaration functionDeclaration : program.getFuncs()) {
+        for (FuncDeclaration functionDeclaration : program.getFuncs())
+        {
             functionDeclaration.accept(this);
         }
 
-        for (var stmt : program.getMain().getMainStatements()) {
-            if(stmt instanceof VarDecStmt) {
+        for (var stmt : program.getMain().getMainStatements())
+        {
+            if(stmt instanceof VarDecStmt || stmt instanceof ArrayDecStmt ||
+                    stmt instanceof ForloopStmt || stmt instanceof ImplicationStmt)
+            {
                 stmt.accept(this);
             }
         }
@@ -41,10 +47,6 @@ public class NameAnalyzer extends Visitor<Void> {
     @Override
     public Void visit(FuncDeclaration funcDeclaration) {
         var functionItem = new FunctionItem(funcDeclaration);
-        var functionSymbolTable = new SymbolTable(SymbolTable.top, funcDeclaration.getName().getName());
-        functionItem.setFunctionSymbolTable(functionSymbolTable);
-        SymbolTable.push(functionSymbolTable);
-
         // ToDo
         boolean isFinished = false;
         while (!isFinished)
@@ -62,13 +64,19 @@ public class NameAnalyzer extends Visitor<Void> {
             }
             counter += 1;
         }
-//
-        for (ArgDeclaration varDeclaration : funcDeclaration.getArgs()) {
+        var functionSymbolTable = new SymbolTable(SymbolTable.top, funcDeclaration.getName().getName());
+        functionItem.setFunctionSymbolTable(functionSymbolTable);
+        SymbolTable.push(functionSymbolTable);
+        for (ArgDeclaration varDeclaration : funcDeclaration.getArgs())
+        {
             varDeclaration.accept(this);
         }
 
-        for (var stmt : funcDeclaration.getStatements()) {
-            if(stmt instanceof VarDecStmt) {
+        for (var stmt : funcDeclaration.getStatements())
+        {
+            if(stmt instanceof VarDecStmt || stmt instanceof ArrayDecStmt ||
+                    stmt instanceof ForloopStmt || stmt instanceof ImplicationStmt)
+            {
                 stmt.accept(this);
             }
         }
@@ -91,7 +99,83 @@ public class NameAnalyzer extends Visitor<Void> {
             VariableRedefinition errorVariableRedefinition = new VariableRedefinition(varDeclaration.getLine(), varDeclaration.getIdentifier().getName());
             this.nameErrors.add(errorVariableRedefinition);
         }
-//
+        return null;
+    }
+    @Override
+    public Void visit(ForloopStmt forLoop) {
+        var forIt = new VariableItem(forLoop.getIterator().getName(), forLoop.getIterator().getType());
+        // ToDo
+        try
+        {
+            SymbolTable.top.put(forIt);
+        }
+        catch (ItemAlreadyExistsException itemAlreadyExistsException)
+        {
+            VariableRedefinition errorValRed = new VariableRedefinition(forLoop.getLine(), forLoop.getIterator().getName());
+            this.nameErrors.add(errorValRed);
+        }
+
+        var loopSymbolTable = new SymbolTable(SymbolTable.top, forLoop.getArrayName().getName());
+        SymbolTable.push(loopSymbolTable);
+        for (var stmt : forLoop.getStatements())
+        {
+            if(stmt instanceof VarDecStmt || stmt instanceof ArrayDecStmt ||
+               stmt instanceof ForloopStmt || stmt instanceof ImplicationStmt)
+            {
+                stmt.accept(this);
+            }
+        }
+        SymbolTable.pop();
+        return null;
+}
+    public Void visit(ArrayDecStmt arrDecStmt) {
+        var varItem = new VariableItem(arrDecStmt.getIdentifier().getName(), arrDecStmt.getType());
+
+        // ToDo
+        try
+        {
+            SymbolTable.top.put(varItem);
+        }
+        catch (ItemAlreadyExistsException alreadyExistsException)
+        {
+            VariableRedefinition errorArrRedefinition = new VariableRedefinition(arrDecStmt.getLine(), arrDecStmt.getIdentifier().getName());
+            this.nameErrors.add(errorArrRedefinition);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ArgDeclaration argDeclaration) {
+        var variableItem = new VariableItem(argDeclaration.getIdentifier().getName(), argDeclaration.getType());
+
+        // ToDo
+        try
+        {
+            SymbolTable.top.put(variableItem);
+        }
+        catch (ItemAlreadyExistsException itemAlreadyExistsException)
+        {
+            VariableRedefinition errorVariableRedefinition = new VariableRedefinition(argDeclaration.getLine(), argDeclaration.getIdentifier().getName());
+            this.nameErrors.add(errorVariableRedefinition);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ImplicationStmt impStmt) {
+        var impSymbolTable = new SymbolTable(SymbolTable.top, "Implication Statement");
+        SymbolTable.push(impSymbolTable);
+
+
+        for (var stmt : impStmt.getStatements())
+        {
+            if (stmt instanceof VarDecStmt || stmt instanceof ArrayDecStmt ||
+                stmt instanceof ForloopStmt || stmt instanceof ImplicationStmt)
+            {
+                stmt.accept(this);
+            }
+        }
+        SymbolTable.pop();
         return null;
     }
 }
